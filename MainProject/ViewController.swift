37aacf8bdd083582f51d361dc9d3e000e265f4fa
yangwei <17605064366@163.com>
2023-03-14 16:04:23 +0800

    3.0.0

--- MainProject/ViewController.swift
+++ MainProject/ViewController.swift
@@ -1,19 +1,31 @@
 //
 //  ViewController.swift
-//  MainProject
 //
-//  Created by admin on 2023/3/9.
+//  Created by MambaYong on 2021/12/7.
+//  Copyright © 2021 MambaYong. All rights reserved.
 //
 
 import UIKit
+import CTMediator
+import A
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        let btn = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        btn.backgroundColor = .red
+        view.addSubview(btn)
+        btn.addTarget(self, action: #selector(btnClick), for: .touchUpInside)
     }
-
-
+    
+    @objc func btnClick() {
+        if let vc = CT().A_demo(name: "你好", callback: { callStr in
+            print(callStr)
+        }) {
+            self.present(vc, animated: true)
+        }
+    }
+    
 }
-
